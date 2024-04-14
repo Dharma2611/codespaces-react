b576f4f4ed1eb9c502401841219badd6e7c194f8
Dharma2611 <53259129+Dharma2611@users.noreply.github.com>
2024-04-14 05:58:53 +0000

    change user in my side

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,17 +1,18 @@
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import  axios from 'axios'
-//let url = 'https://legendary-happiness-759r69p7rrjcwr4-4000.app.github.dev/users'
+let url = 'https://legendary-happiness-759r69p7rrjcwr4-4000.app.github.dev/users'
 const user =()=>{
-    const [users,setUser]= useState(null)
+    const [user,setUser]= useState(null)
     const [userId  ,setUserId]=useState("")
     const [errorMessage , setErrorMessage]= useState("")
     const handleChange=(event)=>{
         setUserId(event.target.value);
 
     }
-    useEffect(()=>{
-        axios.get('/api/users')
+    const handleSubmit=(event)=>{
+        event.preventDefault()
+        axios.get("api/users")
         .then((res)=>{
             setUser(res.data)
         })
@@ -20,20 +21,16 @@ const user =()=>{
             setErrorMessage('something went wrong')
         })
 
-    })
-
-    
+    }
     return(
         <>
-       <div>
-        <h1>get user</h1>
-       </div>
-        {users.map(user=>{
-            <div key={user.id}>
-                <h1>{user.name}</h1>
-
-            </div>
-        })
+        <form onSubmit={handleSubmit}>
+            <p>
+                <input type="text" name="userId"  value={userId} onChange={handleChange}/>
+            </p>
+            <button>getUser</button>
+        </form>
+        {(user)? <div>{user.name}</div> : null
 
         }
         {errorMessage ? <div>{errorMessage}</div>: null}
